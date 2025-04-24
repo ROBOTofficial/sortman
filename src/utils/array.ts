589f6f18ed6672b2ac3225afa30ded701f31e534
ROBOT <tkata12@outlook.jp>
2025-04-25 01:54:35 +0900

    add: desc option in sort check func

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,12 +1,14 @@
 export const dedepulication = <T>(arr: T[]): T[] => Array.from(new Set(arr));
 
-export const sortCheck = (arr: number[]): boolean => {
+export const sortCheck = (arr: number[], desc?: boolean): boolean => {
 	if (!arr.length) return true;
 	for (let i = 0; i < arr.length; i++) {
 		const num = arr[i];
 		const next = arr[i + 1];
 		if (num && next) {
-			if (!(num <= next)) {
+			if (desc && !(num >= next)) {
+				break;
+			} else if (!(num <= next)) {
 				break;
 			}
 		} else {
