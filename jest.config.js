3953dc2207f48beae510a267d84394653da1d42e
ROBOT <tkata12@outlook.jp>
2025-04-25 01:51:21 +0900

    add: test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,16 @@
+import { join } from "node:path";
+
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
+	preset: "ts-jest",
 	testEnvironment: "node",
 	transform: {
-		"^.+.tsx?$": ["ts-jest", {}]
-	}
+		"^.+.tsx?$": [
+			"ts-jest",
+			{
+				tsconfig: join(import.meta.dirname, "jest.tsconfig.json")
+			}
+		]
+	},
+	moduleFileExtensions: ["ts", "js"]
 };
---
+++ tests/utils.ts
@@ -0,0 +1,24 @@
+export const numArrData = [100, 300, 200, 3, 201];
+
+export const objectArrData = [
+	{
+		num: 100,
+		content: "hi!"
+	},
+	{
+		num: 300,
+		content: "hi!!!!"
+	},
+	{
+		num: 200,
+		content: "hi!!"
+	},
+	{
+		num: 3,
+		content: "hi"
+	},
+	{
+		num: 201,
+		content: "hi!!!"
+	}
+];
