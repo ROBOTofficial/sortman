e4be8c58445eb4c8b7dec2b07ee10b2551cc295b
ROBOT <tkata12@outlook.jp>
2025-04-25 18:35:48 +0900

    fix path

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "tsup";
-import { join } from "node:path";
+import path from "node:path";
 
 export default defineConfig({
 	entry: ["src/index.ts"],
 	dts: true,
 	format: ["esm", "cjs"],
-	tsconfig: join(import.meta.dirname, "tsconfig.json"),
+	tsconfig: path.join(import.meta.dirname, "tsconfig.json"),
 	sourcemap: true,
 	minify: true
 });
